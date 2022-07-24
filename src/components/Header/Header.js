c3dec10a49132796840059082550162cdbe35b6c
sakif hridoy <silenticebreaker@gmail.com>
2022-07-25 03:03:43 +0600

    add fontawesome and resize the logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,6 @@ import './Header.css';
 const Header = () => {
     return (
         <div className="header">
-            <h1>This is Header</h1>
             <img className="logo" src={logo} alt="" />
             <nav>
                 <a href="/shop">Shop</a>
